00cbd1a74b9c5479f91b43e1ed30da9da0a557bb
yukiyao119 <ukiyao@hotmail.com>
2019-11-18 16:13:04 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,47 @@
-export default (state = [], action) => {
-  return state;
+import { bindActionCreators } from "redux";
+
+export default function quotes (state = [], action) {
+
+  let idx;
+  let quote;
+  let votes;
+
+  switch (action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+
+    case "REMOVE_QUOTE":
+      let idx = state.findIndex(quote => quote.quoteId === action.id)
+      // return state.filter(quote => quote.quoteId !== action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    case "UPVOTE_QUOTE":
+      // idx = state.findIndex(quote => quote.id === action.quoteId)
+      // quote = state[idx]
+      quote = state.find(quote => quote.id === action.quoteId)
+      idx = state.indexOf(quote)
+      votes = quote.votes + 1
+
+      return [...state.slice(0, idx),
+        Object.assign({}, quote, { votes: votes }),
+        ...state.slice(idx + 1)]
+
+    case "DOWNVOTE_QUOTE":
+      idx = parseInt(state.findIndex(quote => quote.id === action.quoteId));
+      quote = state[idx];
+      votes = quote.votes - 1
+
+        if (quote.votes > 0) {
+          return [...state.slice(0, idx),
+            Object.assign({}, quote, { votes: votes }),
+            ...state.slice(idx + 1)]
+        }
+      return state;
+
+    default: 
+      return state;
+  }
+
 }
+
+
